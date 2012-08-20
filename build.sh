1dc5f10afb067b13753e75d2f9515c7dc8feb7ff
gugmaster <stefan@cocoanetics.com>
2012-08-20 14:17:03 +0200

    removed typo

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 chmod +x *.sh
 ./keychain-prepare.sh %@  || { echo "prepare keychain failed"; exit 1; } 
 ./xcode-build.sh %@ || { echo "xcode-build failed"; exit 1; } 
-./hockeykit-manifesst.sh %@  || { echo "creating hockeykit manifest failed"; exit 1; } 
+./hockeykit-manifest.sh %@  || { echo "creating hockeykit manifest failed"; exit 1; } 
 ./hockeykit-image.sh -o build/Icon.png %@ || { echo "hockeyimage failed"; exit 1; } 
 ./hockeykit-releasenotes.sh %@ || { echo "creating hockeykit releasenotes failed"; exit 1; }
 ./keychain-cleanup.sh %@ || { echo "cleanup keychain failed"; exit 1; }
